6d7f7c769ce54a07f7ec6c061781c285fb656e06
Shemi Nechmad <crunchies@mail.com>
2019-09-01 18:26:54 +0300

    0.11

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Passed change
+          Passed change2
         </a>
       </header>
     </div>
