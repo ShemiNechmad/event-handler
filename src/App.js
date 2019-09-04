70c737894a607bced58e96c9372656988710989c
Shemi Nechmad <crunchies@mail.com>
2019-09-04 04:25:33 +0300

    4.9.11

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+
+
+  componentDidMount(){
+
+  }
+  componentWillUnmount(){
+ 
+  }
+  hambar() {
+    var x = document.getElementById("hamnav");
+    if (x.className === "hamnav") {
+      x.className = "hamnavTrue";
+    } else {
+      x.className = "hamnav";
+    }
+  }
+
+
+  render(){
   return (
-    <div className="App">
-     
-     Shem
-    </div>
+<div>
+      <span>
+        <div className="title">
+          <span className="titlogo">Event Handler</span>
+          <span className="hamburgerbar">
+          <i className="fa fa-bars" onClick={this.hambar}></i>
+          </span>
+
+          
+        </div>
+      </span>  
+      <div className="hamnav" id="hamnav">
+            <a href="#home">Home</a><br/>
+            <a href="#news">News</a><br/>
+            <a href="#contact">Contact</a><br/>
+            <a href="#SignUp">Signup</a><br/>
+            <a href="#Login">Login</a>
+          </div>
+      <span>abc</span>
+  
+
+
+</div>
+
+
   );
+}
 }
 
 export default App;
