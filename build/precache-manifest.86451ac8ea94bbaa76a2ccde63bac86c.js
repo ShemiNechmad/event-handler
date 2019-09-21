e6a049d1103bd135fbc50ce23a83e77657f56c11
Shemi Nechmad <crunchies@mail.com>
2019-09-22 02:50:46 +0300

    with build

---
+++ build/precache-manifest.86451ac8ea94bbaa76a2ccde63bac86c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f017bc805e213e9e66588b5e2bdb33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1798d7f9f9d74512ea32",
+    "url": "/static/css/2.d4c5f937.chunk.css"
+  },
+  {
+    "revision": "4decfa30304caf60f619",
+    "url": "/static/css/main.0824cd87.chunk.css"
+  },
+  {
+    "revision": "1798d7f9f9d74512ea32",
+    "url": "/static/js/2.42a09c35.chunk.js"
+  },
+  {
+    "revision": "4decfa30304caf60f619",
+    "url": "/static/js/main.c4e90e19.chunk.js"
+  },
+  {
+    "revision": "644626e3d475a1782b1e",
+    "url": "/static/js/runtime~main.8d6a1da5.js"
+  },
+  {
+    "revision": "77172b0c6216dee9e8599b79a77cd74e",
+    "url": "/static/media/Table.77172b0c.jpg"
+  },
+  {
+    "revision": "742a4c34068ffeb802232b20e2cb72ed",
+    "url": "/static/media/roundpica.742a4c34.jpg"
+  },
+  {
+    "revision": "199046648606d959da83e5045b81282a",
+    "url": "/static/media/roundpicb.19904664.jpg"
+  },
+  {
+    "revision": "315a0ea528c3cb0212810dba6e0e7a2f",
+    "url": "/static/media/roundpicc.315a0ea5.jpg"
+  }
+]);
